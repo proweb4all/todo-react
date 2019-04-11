e06cb99750d03a0a00ea60edaf5bbe37de0ae042
proweb4all <proweb4all@narod.ru>
2019-04-11 12:59:02 +0300

    25

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log('Yo!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const AppHeader = () => {
+  return <h1>My Todo List</h1>
+}
+const SearchPanel = () => {
+  return <input placeholder='search'/>
+}
+const TodoList = () => {
+  return (
+    <ul>
+    <li>Learn React</li>
+    <li>Build Awesome App</li>
+    </ul>  
+  )
+}
+const App = () => {
+  return (
+    <div>
+      <AppHeader />
+      <SearchPanel />
+      <TodoList />
+    </div>
+  )
+}
+ReactDOM.render(<App />, document.getElementById('root'));
