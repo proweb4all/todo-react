df320fadeeb83d24928fa047d70b012ce2770a5a
proweb4all <proweb4all@narod.ru>
2019-04-15 22:37:29 +0300

    45

--- src/components/add-item/add-item.js
+++ src/components/add-item/add-item.js
@@ -2,19 +2,32 @@ import React, {Component} from 'react';
 import './add-item.css';
 
 export default class AddItem extends Component {
-
+  state = {
+    label: ''
+  }
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.onAdd(this.state.label);
+    this.setState({ label: '' });
+  }
+  onLabelChange = (e) => {
+    this.setState({ label: e.target.value });
+    console.log(e.target.value);
+  }
   render() {
-    const { onAdd } = this.props;
-    let classNames = 'add-item';
     return (
-      <div className={classNames}>
-        <button type="button"
-          className="btn btn-outline-success btn-sm float-right"
-          onClick={() => onAdd('NewItem')}>
+      <form className='item-add-form d-flex'
+        onSubmit={this.onSubmit}>
+        <input type='text' className='form-control'
+          onChange={this.onLabelChange}
+          value={this.state.label}
+          placeholder='What needs to be done' />
+        <button type="submit"
+          className="btn btn-outline-success btn-sm float-right">
           <i className="fa fa-plus" />
           Add Item
         </button>
-      </div>
+      </form>
     );    
   }
 }
--- src/components/item-status-filter/item-status-filter.js
+++ src/components/item-status-filter/item-status-filter.js
@@ -6,7 +6,7 @@ export default class ItemStatusFilter extends Component{
     return (
       <div className="btn-group">
         <button type="button"
-                className="btn btn-info">All</button>
+                className="btn btn-success">All</button>
         <button type="button"
                 className="btn btn-outline-secondary">Active</button>
         <button type="button"
